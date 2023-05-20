ee9e6d66f7cc87734b25449c169e6e29d10f212f
LuisJRS <110069992+LuisJRS@users.noreply.github.com>
2023-05-20 00:16:35 -0300

    Foi acrecentado um compo RG

--- teste.py
+++ teste.py
@@ -3,8 +3,9 @@ while True:
     chave = input('Matricula: ')
     nome = input('Nome: ')
     idade = input('Idade: ')
+    rg = int(input('RG: '))
 
-    dic[chave] = nome,idade 
+    dic[chave] = nome,idade,rg 
     
     deseja = input('Deseja Cadastrar Mais (S/N): ').upper()
     if deseja == 'N':
