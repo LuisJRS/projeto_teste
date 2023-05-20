9df78fc9a74e262d57581e01e4e4315342b01680
LuisJRS <110069992+LuisJRS@users.noreply.github.com>
2023-05-20 00:12:55 -0300

    Update teste.py
    foi adicionado um loop para melhor a dinâmica do sistema

--- teste.py
+++ teste.py
@@ -1,9 +1,14 @@
 dic = {}
+while True:
+    chave = input('Matricula: ')
+    nome = input('Nome: ')
+    idade = input('Idade: ')
 
-chave = input('Matricula: ')
-nome = input('Nome: ')
-idade = input('Idade: ')
-
-dic[chave] = nome,idade 
+    dic[chave] = nome,idade 
+    
+    deseja = input('Deseja Cadastrar Mais (S/N): ').upper()
+    if deseja == 'N':
+        print('Obrigao por Usar Nosso Sistema!!!')
+        break
 
 print(dic)
